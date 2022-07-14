f466fc9e65169f72e856dc81018256a441585f2e
FerBaeza <fbaezahurtado@gmail.com>
2022-07-14 21:26:31 +0200

    install

---
+++ src/database.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const DB_URI = require('./settings/atlas_settings');
+
+//console.log(DB_URI);
+
+
+/**
+ * Conexion MongoDB Atlas
+ */
+ const dbConnect =()=>{
+    mongoose.connect(
+    DB_URI, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+    );
+}
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+console.log("Connected successfully");
+});
+
+
+module.exports = dbConnect;
---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+
+const dbConnect = require('./database')
+
+
+//Settings
+/**
+ **const port = process.env.PORT || 3000 
+ */
+
+app.set('port', process.env.PORT || 3000);
+
+
+//Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+//Routes
+app.use('/api',require('./routes/index'));
+
+
+//StaticFiles
+/**
+ * * Al usar el metodo join, conseguimos hacer nuestro path multiplataforma
+ * console.log(path.join(__dirname, 'public'))   
+ */
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//Server
+app.listen(app.get('port'), ()=>{
+    console.log(`Servidor arrancado en puerto ${app.get('port')}`)
+})
