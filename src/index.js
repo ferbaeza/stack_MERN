ab6f9c29155c8877484f1d7afb0e9ee886f870f2
FerBaeza <fbaezahurtado@gmail.com>
2022-07-18 12:02:55 +0200

    install react

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 require ('dotenv').config();
-const dbConnect = require('./database')
+const dbConnect = require('./settings/database')
 
 
 //Settings
